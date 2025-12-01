c4bc6434a051a312d0648205fc3862c17c03d132
Gerardo González <josegerardogonzalezs@icloud.com>
2025-12-01 08:08:02 -0600

    Se incluye el reporte de práctica

--- Scripts/app.h
+++ Scripts/app.h
@@ -1,9 +1,10 @@
+// --Header File--
+#ifndef APP_H // Si no está definido APP_H
+#define APP_H // Define APP_H
 
-#ifndef APP_H
-#define APP_H
+void calcular(); //Declaramos la función que va a calcular los valores de k
+void simular();  //Declaramos la función que llevará la simulación con los valores obtenidos de k
+void graficar_csv(const char *input_csv, const char *output_png); //Declaramos la función que graficará
+                                                                  //los datos de la simulación
 
-void calcular();
-void simular();
-void graficar_csv(const char *input_csv, const char *output_png);
-
-#endif
+#endif //Cierra la condición que define APP_H
